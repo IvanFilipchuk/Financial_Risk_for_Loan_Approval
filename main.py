bc71688ee0765775adb54c1d489e5fe22757a109
Ivan Filipchuk <ivanflpck@gmial.com>
2024-10-17 14:57:41 +0200

    Add script to introduce random missing values in the dataset and new dataset

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import pandas as pd
+import numpy as np
 
 pd.set_option('display.max_columns', None)
-dataset = pd.read_csv('./Loan.csv')
+
+# Script to create a new dataset with missing values
+
+# original_dataset = pd.read_csv('./Loan.csv')
+# print(original_dataset.head())
+# print(original_dataset.isnull().sum())
+# print(original_dataset.describe())
+#
+# missing_percentage = np.random.uniform(0.10, 0.15)
+# rows, cols = original_dataset.shape
+# num_missing_values = int(missing_percentage * rows * cols)
+#
+# dataset_with_missing = original_dataset.copy()
+#
+# for _ in range(num_missing_values):
+#     row = np.random.randint(0, rows)
+#     col = np.random.randint(0, cols)
+#     dataset_with_missing.iat[row, col] = np.nan
+#
+# dataset_with_missing.to_csv('./Loan_with_missing_data.csv', index=False)
+
+dataset = pd.read_csv('./Loan_with_missing_data.csv')
 print(dataset.head())
+print(dataset.isnull().sum())
+print(dataset.describe())
